0bd136ba81adaf7bdf6d780f447c34885295e9d9
Diego Minelli <diego.minelli@hotmail.com>
2021-09-17 12:06:44 -0300

    Excluindo o cliente

--- admin/controller/listaClientes-controller.js
+++ admin/controller/listaClientes-controller.js
@@ -1,6 +1,6 @@
 import { clienteService } from '../service/cliente-service.js';
 
-const criarNovaLinha = (nome, email) => {
+const criarNovaLinha = (nome, email, id) => {
   const linhaNovoCliente = document.createElement('tr');
   const conteudo = `
         <td class="td" data-td>${nome}</td>
@@ -14,13 +14,29 @@ const criarNovaLinha = (nome, email) => {
 
     `;
   linhaNovoCliente.innerHTML = conteudo;
+  linhaNovoCliente.dataset.id = id;
+
   return linhaNovoCliente;
 };
 
 const tabela = document.querySelector('[data-tabela]');
 
+tabela.addEventListener('click', (evento) => {
+  let ehBotaoDeletar =
+    evento.target.className === 'botao-simples botao-simples--excluir';
+  if (ehBotaoDeletar) {
+    const linhaCliente = evento.target.closest('[data-id]');
+    let id = linhaCliente.dataset.id;
+    clienteService.removeCliente(id).then(() => {
+      linhaCliente.remove();
+    });
+  }
+});
+
 clienteService.listaClientes().then((data) => {
   data.forEach((elemento) => {
-    tabela.appendChild(criarNovaLinha(elemento.nome, elemento.email));
+    tabela.appendChild(
+      criarNovaLinha(elemento.nome, elemento.email, elemento.id)
+    );
   });
 });
